2bc9a8682b53638e8cf80fe99b37fb779caa5f58
Pietro King <pking@slac.stanford.edu>
2023-07-11 18:23:25 -0700

    Added general parameters on ePixUHR viewer for region of interest

--- software/deviceFiles/ePixUhr.py
+++ software/deviceFiles/ePixUhr.py
@@ -13,10 +13,13 @@ class DataReceiverEpixUHR(DataReceiverBase):
     def __init__(self, **kwargs):
         super().__init__(168, 192, **kwargs)
 
-        self.framePixelRow = 168
-        self.framePixelColumn = 192
-        self.counter = 0
-        self.lane_map = self.lane_map()
+        numClusters             = 14
+        numLanes                = 16
+        self.framePixelRow      = numClusters*12
+        self.framePixelColumn   = numLanes*12
+        numPixels               = int(self.framePixelRow * self.framePixelColumn)
+        self.lane_map           = self.lane_map(numClusters, numLanes, numPixels)
+        
 
     def read_uint12(self,data_chunk):
         #https://stackoverflow.com/questions/44735756/python-reading-12-bit-binary-files
@@ -25,7 +28,7 @@ class DataReceiverEpixUHR(DataReceiverBase):
         snd_uint12 = ((mid_uint8 % 16) << 8) + lst_uint8
         return np.reshape(np.concatenate((fst_uint12[:, None], snd_uint12[:, None]), axis=1), 2 * fst_uint12.shape[0])
 
-    def lane_map(self):
+    def lane_map(self, numClusters, numLanes, numPixels):
         column_map = []
         cluster_map = np.empty(72)
         #create the sp map
@@ -45,58 +48,59 @@ class DataReceiverEpixUHR(DataReceiverBase):
         cluster_map = np.reshape(cluster_map, 72)
 
         #create the column map
-        for cluster_n in range (14):
-            column_map = np.append(column_map , (14*cluster_map) + cluster_n)
+        for cluster_n in range (numClusters):
+            column_map = np.append(column_map , (numClusters*cluster_map) + cluster_n)
 
         #cast column_map to int
         column_map=column_map.astype(int)
 
         #create the cluster_columns_map
-        lane_map = np.concatenate([np.reshape(column_map*2,(168,6)),np.reshape((column_map*2)+1,(168,6))],axis =1 )
+        lane_map = np.concatenate([np.reshape(column_map*2,(int(12*numClusters),6)),np.reshape((column_map*2)+1,(int(12*numClusters),6))],axis =1 )
 
         lane_map = np.flip(lane_map,1)
 
         #re-linearize the cluster_columns_map
-        lane_map = np.reshape(lane_map, 2016)
+        lane_map = np.reshape(lane_map, int(numPixels/numLanes))
         
         return lane_map
 
-    def frame_reorder(self,input_frame,lane_map):
-        frame= np.empty([168,1])
+    def frame_reorder(self,input_frame,lane_map, numLanes):
+        frame= np.empty([self.framePixelRow,1])
         frame.fill(np.nan)
 
-        for lane_n in range (16):
+        for lane_n in range (numLanes):
             #taking one lane at the time
             frame_tmp = input_frame[lane_n,1:]
             #using the map
             lane_tmp = frame_tmp[lane_map]
             #reshape
-            lane_tmp = np.reshape(lane_tmp, (168,12))
+            lane_tmp = np.reshape(lane_tmp, (self.framePixelRow,12))
             frame = np.concatenate([frame,lane_tmp],axis =1)
 
         return frame[np.logical_not(np.isnan(frame))]    
 
     
     def descramble(self, frame, GainMSB):
-        # print('Debug: Frame counter {}'.format(self.counter))
-        # self.counter = self.counter + 1
-        
+        #Intial setting
+        numLanes                = 16 
+        numPixels               = int(self.framePixelRow * self.framePixelColumn)
         #create the frames
         current_frame_temp = np.zeros((self.framePixelRow, self.framePixelColumn), dtype=int)
-        rawData_12bit = np.empty((16,2017), dtype=int)
+        rawData_12bit = np.empty((numLanes,int(numPixels/numLanes)+1), dtype=int)
 
         #get the frames from the stream
         rawData_8bit = frame.getNumpy(0, frame.getPayload()).view(np.uint8)
-        rawData_8bit = np.reshape(rawData_8bit[16:48424],(2017,24))
+        rawData_8bit = np.reshape(rawData_8bit[16:48424],(int(numPixels/numLanes)+1,24))
+        # rawData_8bit = np.reshape(rawData_8bit[16:13864],(int(numPixels/numLanes)+1,24))
         rawData_8bit = np.flip(rawData_8bit,1).T
 
         #parse the 8bit chunks into the 12bit
-        for j in range (2017):
+        for j in range (int(numPixels/numLanes)+1):
             rawData_12bit[:,j] = self.read_uint12(rawData_8bit[:,j])
 
         #use the map to filter the pixels
-        frame = self.frame_reorder(rawData_12bit, self.lane_map)
-        frame = np.reshape(frame, (168,192))
+        frame = self.frame_reorder(rawData_12bit, self.lane_map, numLanes)
+        frame = np.reshape(frame, (self.framePixelRow,self.framePixelColumn))
         frame = frame.astype(int)
 
         current_frame_temp = np.flip(np.flip(frame,0),1)
